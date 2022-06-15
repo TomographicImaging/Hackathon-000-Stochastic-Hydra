f48e43f4eba3d52e4193046423fdcc0b0f771020
Imraj Singh <rmapsin@ucl.ac.uk>
2022-06-15 11:09:08 +0100

    Adding the setup and re-ordering

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='HydraHackathon',
+      version='1.0',
+      description='Hydra Hackathon Implementation',
+      author='Imraj Singh and Claire Deplanke etc',
+      author_email='rmapsin@ucl.ac.uk',
+      url='N/A',
+      packages=['src'],
+      )
