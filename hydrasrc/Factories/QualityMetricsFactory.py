da5aefb009079a8bd235890759c94a6034f59f7d
Imraj Singh <rmapsin@ucl.ac.uk>
2022-06-22 00:52:44 +0100

    Adding MSE, MAE and PSNR into the Quality factory

--- hydrasrc/Factories/QualityMetricsFactory.py
+++ hydrasrc/Factories/QualityMetricsFactory.py
@@ -1,14 +1,14 @@
 import sirf.STIR as pet
-import ImageQualityCallback
-from ImageQualityCallback import MSE, MAE, PSNR, ImageQualityCallback
+from ImageQualityCallback import ImageQualityCallback
 from datetime import datetime
 import tensorboardX
+import numpy as np
 
 
 class QualityMetricsFactory(object):
     def __init__(self, cfg):
         self.cfg = cfg
-        self.metrics_dict = {'MSE':MSE, 'MAE':MAE, 'PSNR':PSNR}
+        self.metrics_dict = {'MSE':self.MSE, 'MAE':self.MAE, 'PSNR':self.PSNR}
         self.statistics_dict = {'MEAN': (lambda x: x.mean()),
                                     'STDDEV': (lambda x: x.std())}
         dt_string = datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -22,4 +22,27 @@ class QualityMetricsFactory(object):
             roi_mask_dict = dataset.roi_mask_dict, 
             metrics_dict = self.metrics_dict, 
             statistics_dict = self.statistics_dict)
+
+    def MSE(self, x,y):
+        """ mean squared error between two numpy arrays
+        """
+        return ((x-y)**2).mean()
+
+    def MAE(self, x,y):
+        """ mean absolute error between two numpy arrays
+        """
+        return np.abs(x-y).mean()
+
+    def PSNR(self, x, y, scale = None):
+        """ peak signal to noise ratio between two numpy arrays x and y
+            y is considered to be the reference array and the default scale
+            needed for the PSNR is assumed to be the max of this array
+        """
+    
+        mse = ((x-y)**2).mean()
+    
+        if scale == None:
+            scale = y.max()
+    
+        return 10*np.log10((scale**2) / mse)
 
